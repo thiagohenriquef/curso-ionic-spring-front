a2ee18cbd91b7fea5705c2ace3c628d4913c1c99
Thiago Henrique <thiagoferreira@softbox.com.br>
2019-05-17 17:47:25 -0300

    pagina de checkout, confirmação de pedido, seleção de endereço e salvar pedido

---
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -0,0 +1,75 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartItem } from '../../models/cart-item';
+import { CartService } from '../../services/domain/cart.service';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { EnderecoDTO } from '../../models/endereco.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoService } from '../../services/domain/pedido.service';
+
+/**
+ * Generated class for the OrderConfirmationPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-order-confirmation',
+  templateUrl: 'order-confirmation.html',
+})
+export class OrderConfirmationPage {
+
+  pedido: PedidoDTO;
+  cartItems: CartItem[];
+  cliente: ClienteDTO;
+  endereco: EnderecoDTO;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public pedidoService: PedidoService,
+    public cartService: CartService,
+    public clienteService: ClienteService) {
+
+    this.pedido = this.navParams.get('pedido');
+  }
+
+  ionViewDidLoad() {
+    this.cartItems = this.cartService.getCart().itens;
+
+    this.clienteService.findById(this.pedido.cliente.id)
+      .subscribe(response => {
+        this.cliente = response as ClienteDTO;
+        this.endereco = this.findEndereco(this.pedido.enderecoDeEntrega.id, response['enderecos']);
+      }, error => this.navCtrl.setRoot('HomePage'));
+  }
+
+  private findEndereco(id: string, list: EnderecoDTO[]) : EnderecoDTO {
+    const position = list.findIndex(x => x.id == id);
+    return list[position];
+  }
+
+  total() : number {
+    return this.cartService.total();
+  }
+
+  checkout() {
+    this.pedidoService.insert(this.pedido)
+      .subscribe(response => {
+        this.cartService.createOrClearCart();
+        console.log(response.headers.get('location'));
+      }, error => {
+        if (error.status == 403) {
+          this.navCtrl.setRoot('HomePage');
+        }
+      })
+  }
+
+  back() {
+    this.navCtrl.setRoot('CartPage');
+  }
+
+}
--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+import { StorageService } from '../../services/storage.service';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartService } from '../../services/domain/cart.service';
 
 @IonicPage()
 @Component({
@@ -10,45 +14,42 @@ import { EnderecoDTO } from '../../models/endereco.dto';
 export class PickAddressPage {
 
   items: EnderecoDTO[];
+  pedido: PedidoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public clienteService: ClienteService, public storage: StorageService, public cartService: CartService) {
   }
 
   ionViewDidLoad() {
-    this.items = [
-      {
-        id: "1",
-        logradouro: "Rua Quinze de Novembro",
-        numero: "300",
-        complemento: "Apto 200",
-        bairro: "Santa Mônica",
-        cep: "48293822",
-        cidade: {
-          id: "1",
-          nome: "Uberlândia",
-          estado: {
-            id: "1",
-            nome: "Minas Gerais"
+    const localUser = this.storage.getLocalUser();
+    if (localUser && localUser.email && localUser.email.sub) {
+      this.clienteService.findByEmail(localUser.email.sub)
+        .subscribe(response => {
+          this.items = response['enderecos'];
+          const cart = this.cartService.getCart();
+
+          this.pedido = {
+            cliente: {
+              id: response['id']
+            },
+            enderecoDeEntrega: null,
+            pagamento: null,
+            itens: cart.itens.map(el => ({
+              quantidade: el.quantidade,
+              produto: { id: el.produto.id }
+            }))
           }
-        }
-      },
-      {
-        id: "2",
-        logradouro: "Rua Alexandre Toledo da Silva",
-        numero: "405",
-        complemento: null,
-        bairro: "Centro",
-        cep: "88933822",
-        cidade: {
-          id: "3",
-          nome: "São Paulo",
-          estado: {
-            id: "2",
-            nome: "São Paulo"
+        }, error => {
+          if (error.status === 403) {
+            this.navCtrl.setRoot('HomePage');
           }
-        }
-      }
-    ];
+        });
+    } else {
+      this.navCtrl.setRoot('HomePage');
+    }
   }
 
+  public nextPage(item: EnderecoDTO) {
+    this.pedido.enderecoDeEntrega = { id: item.id };
+    this.navCtrl.push('PaymentPage', { pedido: this.pedido });
+  }
 }
